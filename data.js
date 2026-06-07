bfef028e34ab43a74de64fde524fa04d4b126cb8
twentyfirst-century-boy <gungungupta94501@gmail.com>
2026-06-08 01:55:55 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,17 @@
-let defaultDramas = [
+let dramas = [
 {
   id: 1,
   title: "Hidden Love",
   category: "C-Drama",
   rating: "9.7",
-  image: "https://graph.org/file/b0aa2135f5b872d6ff3f3-03732df180ff0a4bbe.jpg",
-  description: "A romantic story about love and emotions."
+  image: "https://graph.org/file/....jpg",
+
+  // 🔥 NEW DETAILS ADD HERE
+  description: "A romantic story",
+  year: "2023",
+  episodes: 24,
+  genre: "Romance, Drama",
+  status: "Completed",
+  about: "Ye ek beautiful love story hai jo school life se start hoti hai."
 }
 ];
-
-let stored = JSON.parse(localStorage.getItem("dramas"));
-
-const dramas = stored && stored.length ? stored : defaultDramas;
